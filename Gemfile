f67e38255d18ff3a2541ac47b603b50ffaa18c67
yanina-lara <yanina-lara@hotmail.com>
2024-04-14 23:28:00 -0300

    Cors added

--- Gemfile
+++ Gemfile
@@ -7,9 +7,11 @@ gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
+gem 'rack-cors'
 gem 'active_model_serializers'
 gem 'rest-client'
 gem 'will_paginate', '~> 3.3'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:3000",
+            "http://127.0.0.1:3000"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      max_age: 86400
+  end
+end
